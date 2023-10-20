17cad1f8414a1d07d9f06d501060d378342efa2b
Rishabh Jain <jainrishabh3007@gmail.com>
2023-10-20 19:16:19 +0530

    added landing page and navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Body />
+      {/* <Header />
+      <Body /> */}
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/body" element={<Body />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import styles from "./Header.module.css";
 import resume from "../../assets/resume.svg";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <p className={styles.heading}>
-          A <span>Resume</span> that stands out!
-        </p>
-        <p className={styles.heading}>
-          Create a resume that is <span>perfect</span> for you.
-        </p>
-      </div>
-        <div className={styles.right}>
-            <img
-            src={resume} alt="resume"
-            />
+      <div className={styles.box}>
+        <div className={styles.left}>
+          <p className={styles.heading}>
+            A <span>Resume</span> that stands out!
+          </p>
+          <p className={styles.heading}>
+            Create a resume that is <span>perfect</span> for you.
+          </p>
         </div>
+        <div className={styles.right}>
+          <img src={resume} alt="resume" />
+        </div>
+      </div>
+
+      <div className={styles.bodyButton}>
+        <button className={styles.button}>
+          <NavLink to="/body" className={styles.navlink}>
+            Start Building
+          </NavLink>
+        </button>
+      </div>
     </div>
   );
 }
